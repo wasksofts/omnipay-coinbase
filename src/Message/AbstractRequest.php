3c2d94f35ce5090e198dedaa5582e92f52855c01
wasksofts <stevenkamanu@gmail.com>
2020-04-03 10:28:23 +0300

    Update AbstractRequest.php

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -5,7 +5,7 @@ namespace Omnipay\CoinBase\Message;
 abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
 {
 
-    protected $endpoint = 'https://api.commerce.coinbase.com/';
+    protected $endpoint ='https://api.commerce.coinbase.com/';
 
     public function getApiKey()
     {
@@ -57,15 +57,26 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         return $this->setParameter('pricing_type', $value);
     }
 
-    public function getLocalPrice()
+    public function getRequestedInfo()
     {
-        return $this->getParameter('local_price');
+        return $this->getParameter('requested_info');
     }
 
-    public function setLocalPrice($value)
+    public function setRequestedInfo($value)
     {
-        return $this->setParameter('local_price', $value);
+        return $this->setParameter('requested_info', $value);
     }
+
+    public function getRedirectUrl()
+    {
+        return $this->getParameter('redirect_url');
+    }
+
+    public function setRedirectUrl($value)
+    {
+        return $this->setParameter('redirect_url', $value);
+    }
+
     protected function getItemData()
     {
         $data = array();
@@ -82,53 +93,10 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         return $this->getTestMode() ?  $this->endpoint : $this->endpoint;
     }
 
-    public function sendData($data)
-    {
-        $body = $data ? json_encode($data) : null;
-
-        $headers = [];
-        $headers['X-CC-Api-Key'] = $this->getApiKey();
-        $headers['X-CC-Version'] = "";
-        $headers['Content-Type'] = 'application/json';
-
-        $httpresponse = $this->httpClient->request($this->getHttpMethod(), $this->getEndpoint(), $headers, $body);
-        var_dump($httpresponse->getBody()->getContents());
-        var_dump($body);
-    }
-
-    public function sendDataNot($data)
-    {
-        $body = $data ? json_encode($data) : null;
-        $url = $this->endpoint;
-
-        $headers = [];
-        $headers['CB-ACCESS-KEY'] = $this->getApiKey();
-        $headers['CB-ACCESS-SIGN'] = $this->generateSignature($url, $body, false, $this->getHttpMethod());
-        $headers['CB-ACCESS-TIMESTAMP'] = time();
-        $headers['Content-Type'] = 'application/x-www-form-urlencoded';
-
-        if ($this->getHttpMethod() == 'POST') {
-            $headers['Content-Type'] = 'application/json';
-        }
-
-        $httpresponse = $this->httpClient->request($this->getHttpMethod(), $this->getEndpoint(), $headers, $body);
-
-        var_dump($httpresponse->getBody()->getContents());
-        // return $this->response = $this->createResponse(json_decode($httpresponse->getBody()->getContents(), true), $httpresponse->getStatusCode());  
-    }
-
     protected function createResponse($data, $statusCode)
     {
         return $this->response = new PurchaseResponse($this, $data, $statusCode);
     }
 
-    public function generateSignature($request_path = '', $body = '', $timestamp = false, $method = 'GET')
-    {
-        $body = is_array($body) ? json_encode($body) : $body;
-        $timestamp = $timestamp ? $timestamp : time();
-
-        $what = $timestamp . $method . $request_path . $body;
-
-        return base64_encode(hash_hmac("sha256", $what, base64_decode($this->getSecret()), true));
-    }
+  
 }
